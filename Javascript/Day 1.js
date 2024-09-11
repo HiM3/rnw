e932cbaaedc438c8ccee176a4e462e103da29048
HiM3 <krmehta1452@gmail.com>
2024-09-11 09:51:30 +0530

    first commit

---
+++ Javascript/Day 1.js
@@ -0,0 +1,22 @@
+alert("hello");
+alert(5+5);
+console.log(5+5);
+document.write("hello<br>");
+document.write("<h1>Hello</h1><br>");
+
+//sum=10
+document.write("Sum = ");
+document.write(5+5);
+
+document.write("<br> add=",5+5+"<br>");
+document.write("<br> sub=",5-5+"<br>");
+
+document.write(5+5+"5");//105
+//++ addition or concat
+console.log(5+"5"+5);
+console.log(5+'5'+5+5);
+console.log("add ="+5+5);
+console.log("5"-"2");
+console.log(5-4);
+console.log("5"-4);
+console.log("hello"-4);
---
+++ Javascript/Day 2.js
@@ -0,0 +1,9 @@
+var value1=40;
+var value2=30.5;
+var T=true;
+var F=false;
+var name1="Meet";
+var surnmae='Mehta';
+console.log(typeof name1);
+console.log(typeof (surnmae));
+console.log(value1 + value2);
