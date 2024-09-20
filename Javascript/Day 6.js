922b0f6c8262bf3f898151ce2fe5649a98b00970
HiM3 <krmehta1452@gmail.com>
2024-09-20 16:47:28 +0530

    first commit

---
+++ Javascript/Day 6.js
@@ -0,0 +1,29 @@
+var old_password = "dhwanit";
+        var password = prompt("Enter your Password:");
+       if(password !=""){
+            if(old_password == password){
+                var new_password = prompt("Enter Your New Password:");
+                if(new_password != ""){
+                  if(new_password != old_password ){
+                    var conform_password = prompt("Enter The Conform Password:")
+                    if(conform_password !=""){
+                        if(new_password == conform_password){
+                            alert("Your Password Has been changed");
+                        }
+                        else{
+                            alert("Your conform password is not matched to the new password");
+                        }
+                    }else("Enter the Conform password");
+                  }else{
+                    alert("Enter the New Password");
+                  }
+                }else{
+                    alert("Enter the new password");
+                }
+            }else{
+                alert("Your password is not matching to your old password");
+            }
+       }
+    else{
+        alert("Enter your correct password");
+    }
