cc57c88798c196608beb58bd75fb6b42af6eefa1
HiM3 <krmehta1452@gmail.com>
2024-09-20 16:41:10 +0530

    first commit

--- Javascript/prb.js
+++ Javascript/prb.js
@@ -3,8 +3,8 @@ a=100;
 if(a<0){
     console.log("less than zero");
 }
-else if(a>=100){
-    console.log("More than 10000");
+else if(a>100){
+    console.log("More than 100");
 }
 else{
     console.log("between 0 to 100");
