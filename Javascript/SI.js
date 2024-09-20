4ca6431a6e8dac62985ffa2bfb847002aef66809
HiM3 <krmehta1452@gmail.com>
2024-09-20 14:55:09 +0530

    first commit

---
+++ Javascript/SI.js
@@ -0,0 +1,6 @@
+var p=100000;
+var r=2.01;
+var n=2;
+var si;
+si=(p*r*n)
+console.log(si/100);
---
+++ Javascript/prb.js
@@ -0,0 +1,11 @@
+var a;
+a=100;
+if(a<0){
+    console.log("less than zero");
+}
+else if(a>=100){
+    console.log("More than 10000");
+}
+else{
+    console.log("between 0 to 100");
+}
