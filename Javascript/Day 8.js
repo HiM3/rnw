54269818f911579c53e89cb6d59b55b2304850cb
HiM3 <krmehta1452@gmail.com>
2024-09-25 16:46:56 +0530

    first commit

---
+++ Javascript/Day 8.js
@@ -0,0 +1,20 @@
+var i,j;
+for(i=1;i<=4;i++){
+    for(j=1;j<=i;j++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
+for(i=1;i<=4;i++){
+    for(j=3;j>=i;j--){
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+for(i=1;i<=4;i++){
+    for(j=1;j<=4;j++){
+        document.write("*");
+    }
+    document.write("<br>")
+}
