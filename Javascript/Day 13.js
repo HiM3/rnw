fdc07478dd22bbfa123ae7a50bcb787f358e152d
HiM3 <krmehta1452@gmail.com>
2024-10-11 14:30:11 +0530

    first commit

---
+++ Javascript/Day 13.js
@@ -0,0 +1,8 @@
+var i,sum=0;
+const arr1=[11,55,66,9,10,99];
+for(i=0;i<arr1.length;i++){
+    arr1[i];
+}
+for(i=0;i<arr1.length;i++){
+    sum+=arr1[i];
+}console.log(arr1);
---
+++ Javascript/arr_methods.js
@@ -0,0 +1,4 @@
+const arr1=[11,22,33,44,55,66]
+//Push Method
+arr1.push(10)
+document.write(arr1)
