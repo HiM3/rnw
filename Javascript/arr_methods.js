d71380b56d34295ada596934de8ba3e6350d2111
HiM3 <krmehta1452@gmail.com>
2024-10-11 16:18:50 +0530

    first commit

--- Javascript/arr_methods.js
+++ Javascript/arr_methods.js
@@ -1,4 +1,25 @@
 const arr1=[11,22,33,44,55,66]
 //Push Method
 arr1.push(10)
-document.write(arr1)
+document.write(arr1,"<br>")
+//Pop Method
+arr1.pop(10)
+document.write(arr1,"<br>")
+//shift method
+arr1.shift()
+document.write(arr1,"<br>")
+//unshift method
+arr1.unshift(11)
+document.write(arr1,"<br>")
+arr1.unshift(13,12,14)
+document.write(arr1,"<br>")
+//Slice Method
+var arr2=[11,1,99,7,6,5,9]
+arr2.splice(3,1)
+document.write(arr2,"<br>")
+arr2.splice(3,0,100)
+document.write(arr2,"<br>")
+arr2.splice(2,1,2)
+document.write(arr2,"<br>")
+arr2.splice(0,7,1,2,3,4,5)
+document.write(arr2,"<br>")
